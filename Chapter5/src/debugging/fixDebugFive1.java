217f2d47b7f9123960ebb52e9140f04774730206
jthai2 <jimmythai35@gmail.com>
2017-09-22 01:58:37 -0500

    update debug1

--- Chapter5/src/debugging/fixDebugFive1.java
+++ Chapter5/src/debugging/fixDebugFive1.java
@@ -1,10 +1,33 @@
 package debugging;
 
-public class fixDebugFive1 {
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class FixDebugFive1
+{
+   public static void main(String args[]) throws Exception
+   {
+      final double HIGH_PRICE = 2.59;
+      final double MED_PRICE = 1.99;
+      final double LOW_PRICE = 0.89;
+      String usersChoiceString;
+      int usersChoice;
+      double bill = 0.0;
+      usersChoiceString = JOptionPane.showInputDialog(null,
+         "Order please\n1 - Burger\2 - Hotdog" +
+         "\n3 - Grilled cheese\4 - Fish sandwich");
+      usersChoice= Integer.parseInt(usersChoiceString);
+      if(usersChoice == 1 || usersChoice == 2) {
+         bill = bill +HIGH_PRICE;
+      }
+      else
+      {
+         bill = bill + MED_PRICE;
+         }
+      usersChoiceString = JOptionPane.showInputDialog(null,
+          "Fries with that?\n1 - Yes\n2 - No");
+      usersChoice = Integer.parseInt(usersChoiceString);
+      if (usersChoice == 1)
+          bill = bill + LOW_PRICE;
+      JOptionPane.showMessageDialog(null,"Bill is " + bill);
+   }
 }
